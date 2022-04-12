d77d3809c99d2dec39aaa0c51881a703e9a42abd
Nathany <naty_om_3@hotmail.com>
2022-04-12 17:25:14 -0300

    parte avaliacao finalizada

--- src/main/java/br/com/empresa/resource/AvaliacaoResource.java
+++ src/main/java/br/com/empresa/resource/AvaliacaoResource.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.empresa.entity.Aluno;
+import br.com.empresa.entity.AlunoDisciplina;
 import br.com.empresa.entity.Avaliacao;
+import br.com.empresa.entity.Disciplina;
 import br.com.empresa.service.AvaliacaoService;
 
 @RestController
@@ -35,6 +39,22 @@ public class AvaliacaoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objAvaliacao.getAlunoDisciplina()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+	@RequestMapping(value="/{idAluno}/{idDisciplina}", method=RequestMethod.GET)
+	public ResponseEntity <Avaliacao> buscarAvaliacaoPorDisciplina(@PathVariable Integer idAluno, @PathVariable Integer idDisciplina){
+		Aluno aluno = new Aluno ();
+		aluno.setId(idAluno);
+		
+		Disciplina disciplina = new Disciplina();
+		disciplina.setId(idDisciplina);
+		
+		
+		AlunoDisciplina alunoDisciplina = new AlunoDisciplina();
+		alunoDisciplina.setAluno(aluno);
+		alunoDisciplina.setDisciplina(disciplina);
+		
+		service.buscarNotaAlunoDisciplina(alunoDisciplina);
+		Avaliacao avaliacao = service.buscarNotaAlunoDisciplina(alunoDisciplina);
+		return ResponseEntity.ok().body(avaliacao);
+	}
 	
 }
